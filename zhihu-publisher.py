05a75e2d5b921ed7679612ae2158ade0249c8155
miracleyoo <miracleyoo@163.com>
2019-12-15 10:48:13 -0500

    update file 如何无换行符复制PDF（论文）中的多行内容

--- zhihu-publisher.py
+++ zhihu-publisher.py
@@ -57,6 +57,7 @@ def reduce_image_size():
     if not os.path.exists(str(image_folder_new_path)): 
         os.mkdir(str(image_folder_new_path))
     for image_path in [image_folder_path.parent/i for i in list(image_folder_path.iterdir()) if not (image_folder_path.parent/i).name.startswith(".") and (image_folder_path.parent/i).is_file()]:
+        print(image_path)
         if os.path.getsize(image_path)>5e5:
             img = Image.open(image_path)
             img.save(image_folder_new_path/image_path.name, optimize=True,quality=85)
